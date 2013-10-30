fe6f30384903c8e9a5680f0882adf1f8fc162292
bricejlin <bricejlin@gmail.com>
2013-10-30 15:56:09 -0400

    add controller spec

---
+++ spec/controllers/restaurant_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe RestaurantsController do
+	describe "GET #index" do
+		it "responds successfully with an HTTP 200 status code" do
+			get :index
+			expect(response).to be_success
+			expect(response.status).to eq(200)
+		end
+
+		it "renders index template" do
+			get :index
+			expect(response).to render_template("index")
+		end
+	end
+
+	describe "GET #new" do
+		it "responds successfully with an HTTP 200 status code" do
+			get :new
+			expect(response).to be_success
+			expect(response.status).to eq(200)
+		end
+
+		it "renders index template" do
+			get :new
+			expect(response).to render_template("new")
+		end
+	end
+end
