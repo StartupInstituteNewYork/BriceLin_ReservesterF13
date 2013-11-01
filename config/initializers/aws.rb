53c94e3094fd71287d6aa7c2e6251754a8d231e8
bricejlin <bricejlin@gmail.com>
2013-11-01 00:03:06 -0400

    change keys

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
+  :aws_access_key_id     => ENV['S3_KEY'],
+  :aws_secret_access_key => ENV['S3_SECRET']
 )
