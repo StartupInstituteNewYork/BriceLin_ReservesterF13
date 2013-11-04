7a92c711a451aca06c84f79005bda999bc8c643f
bricejlin <bricejlin@gmail.com>
2013-11-04 16:05:08 -0500

    add find_on_google_map helper

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,5 @@
 module RestaurantsHelper
+	def find_on_google_map(restaurant)
+		"http://maps.google.com/maps/api/staticmap?visual_refresh=true&size=450x300&sensor=false&zoom=16&markers=#{restaurant.address}"
+	end
 end
